12ba9101c837db28b228427542f1aa5abd470217
Administrator <qwe@123>
2023-12-01 18:59:38 +0800

    new repositories

---
+++ com/ittest/demo1/ittest.java
@@ -0,0 +1,35 @@
+package com.ittest.demo1;
+
+public class ittest {
+    public static void main(String[] args) {
+        System.out.println("ITTEST");
+    }
+}
+//写一个漂亮的程序，要求：web登陆页面
+class LoginPage {
+    public static void main(String[] args) {
+        System.out.println("欢迎来到ITTEST");
+        System.out.println("请输入用户名：");
+        String username = scan.nextLine();
+        System.out.println("请输入密码：");
+        String password = scan.nextLine();
+        if (username.equals("ittest") && password.equals("123456")) {
+            System.out.println("登录成功");
+        }
+    }
+}
+
+//检查cpu/内存/硬盘使用率
+class CheckResource {
+    public static void main(String[] args) {
+        System.out.println("检查资源");
+        System.out.println("CPU使用率：");
+        double cpu = 0.5;
+        System.out.println("内存使用率：");
+        double memory = 0.5;
+        System.out.println("硬盘使用率：");
+        double disk = 0.5;
+        if ()
+    }
+}
+
---
+++ untitled/src/Main.java
@@ -0,0 +1,69 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+//创建一个漂亮的web登陆页面
+// Path: untitled/src/Main.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+//check cpu
+
+// Path: untitled/src/Main.java
+
+//写一个漂亮的登陆页面
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+public class Main {
+    public static void main(
+            public class Main {
+    )
+}
+//写一个漂亮的登陆页面
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+//写一个漂亮的登陆页面
+//检查cpu
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+    // 检查硬盘I/O
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+}
+
+
+
+
+
+//检查cpu
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+}
+/
